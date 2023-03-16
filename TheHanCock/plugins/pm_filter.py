1b999ab6f7450d45f836b568ffd8464894616173
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-03-16 14:46:31 +0530

    Update pm_filter.py

--- TheHanCock/plugins/pm_filter.py
+++ TheHanCock/plugins/pm_filter.py
@@ -30,7 +30,7 @@ async def group(client, message):
                 )             
             for file in files:
                 file_id = file.file_id
-                filename = f"{get_size(file.file_size)} ╸{file.file_name}"
+                filename = f"{get_size(file.file_size)} ϟ {file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=await get_shortlink(f"https://telegram.dog/{sumit}?start=subinps_-_-_-_{file_id}")),]
                 )
