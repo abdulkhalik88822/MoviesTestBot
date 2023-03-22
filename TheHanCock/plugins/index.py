d2a63e53575d23c18c5875aa83c45b47bf3bb64e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-03-22 12:52:35 +0530

    Update index.py

--- TheHanCock/plugins/index.py
+++ TheHanCock/plugins/index.py
@@ -15,20 +15,20 @@ lock = asyncio.Lock()
 @Client.on_message(filters.private & filters.user(ADMINS) & filters.command('index'))
 async def batch(client: Client, message: Message):
     if lock.locked():
-        await message.reply('Wait until previous process complete.')
+        await message.reply("<code>ᴡᴀɪᴛ ᴜɴᴛɪʟ ᴘʀᴇᴠɪᴏᴜs ᴘʀᴏᴄᴇss ᴄᴏᴍᴘʟᴇᴛᴇ.</code>")
     else:
         while True:
-            last_msg = await client.ask(text = "ғᴏʀᴡᴀʀᴅ ғɪʀsᴛ ᴍsɢ ғʀᴏᴍ ᴅʙ ᴄʜᴀɴɴᴇʟ (ᴡɪᴛʜ ǫᴜᴏᴛᴇs) \n\nᴏʀ sᴇɴᴅ ᴛʜᴇ ᴅʙ ᴄʜᴀɴɴᴇʟ ᴘᴏsᴛ ʟɪɴᴋ", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)        
+            last_msg = await client.ask(text = "<code>ғᴏʀᴡᴀʀᴅ ʟᴀsᴛ ᴍsɢ ғʀᴏᴍ ᴅʙ ᴄʜᴀɴɴᴇʟ (ᴡɪᴛʜ ǫᴜᴏᴛᴇs) \n\nᴏʀ sᴇɴᴅ ᴛʜᴇ ᴅʙ ᴄʜᴀɴɴᴇʟ ᴘᴏsᴛ ʟɪɴᴋ</code>", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)        
             try:
                 last_msg_id = last_msg.forward_from_message_id
                 chat_id = last_msg.forward_from_chat.username or last_msg.forward_from_chat.id
                 await client.get_messages(chat_id, last_msg_id)
                 break
             except Exception as e:
-                await last_msg.reply_text(f"This Is An Invalid Message, Either the channel is private and bot is not an admin in the forwarded chat, or you forwarded message as copy.\nError caused Due to <code>{e}</code>")
+                await last_msg.reply_text(f"<code>ᴛʜɪs ɪs ᴀɴ ɪɴᴠᴀʟɪᴅ ᴍᴇssᴀɢᴇ, ᴇɪᴛʜᴇʀ ᴛʜᴇ ᴄᴀʜɴɴᴇʟ ɪs ᴘʀɪᴠᴀᴛᴇ ᴀɴᴅ ʙᴏᴛ ɪs ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀᴛ, ᴏʀ ʏᴏᴜ ғᴏʀᴡᴀʀᴅᴇᴅ ᴍᴇssᴀɢᴇs ᴀs ᴄᴏᴘʏ.\nᴇʀʀᴏʀ ᴄᴀᴜsᴇᴅ ᴅᴜᴇ ᴛᴏ </code> <code>{e}</code>")
                 continue
 
-        msg = await message.reply('Processing...⏳')
+        msg = await message.reply('ᴘʀᴏᴄᴇssɪɴɢ...⏳')
         total_files = 0
         async with lock:
             try:
@@ -63,7 +63,7 @@ async def batch(client: Client, message: Message):
                     current+=1
                     nyav+=1
                     if nyav == 20:
-                        await msg.edit(f"Total messages fetched: {current}\nTotal messages saved: {total_files}")
+                        await msg.edit(f"🖨️ ᴛᴏᴛᴀʟ ᴍᴇssᴀɢᴇs ғᴇᴛᴄʜᴇᴅ|| {current}\n🎬 ᴛᴏᴛᴀʟ ᴍᴇssᴀɢᴇs sᴀᴠᴇᴅ|| {total_files}")
                         nyav -= 20
                     if current == total:
                         break
@@ -73,4 +73,4 @@ async def batch(client: Client, message: Message):
                 logger.exception(e)
                 await msg.edit(f'Error: {e}')
             else:
-                await msg.edit(f'Total {total_files} Saved To DataBase!')
+                await msg.edit(f"🎬 ᴛᴏᴛᴀʟ {total_files} sᴀᴠᴇᴅ ᴛᴏ ᴅᴀᴛᴀʙᴀsᴇ")
