dc8483f99b38e4981e6d75da3f5d85322614f8f5
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-03-29 23:11:39 +0530

    Update pm_filter.py

--- TheHanCock/plugins/pm_filter.py
+++ TheHanCock/plugins/pm_filter.py
@@ -26,8 +26,10 @@ async def group(client, message):
             btn.append(
                     [
                         InlineKeyboardButton("✅ ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ✅", url=f"https://t.me/TheMoviesUpdate/4"),
+                    ],
+                    [
                                         
-                        InlineKeyboardButton("⊙ ᴅɪʀᴇᴄᴛ ғɪʟᴇ|ᴄʟɪᴄᴋ ʜᴇʀᴇ ⊙", url=f"https://t.me/TheMoviesUpdate/161")
+                        InlineKeyboardButton("⊙ ᴅɪʀᴇᴄᴛ ғɪʟᴇ|ᴄʟɪᴄᴋ ʜᴇʀᴇ ⊙", url=f"https://t.me/TheMoviesUpdate/161"),
                     ] 
                     
                 )             
