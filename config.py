b5e57b122ace15937422a9627f42f0629284e323
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-03-30 11:57:31 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -20,19 +20,19 @@ BROADCAST_ADMIN_ID = [5709622852]
 # --------- ᴍᴏɴɢᴏ ᴅʙ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ---------
 DATABASE_URI = "mongodb+srv://MrsFallenBot:MrsFallenBot@cluster0.hsedwn2.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = "Cluster0"
-COLLECTION_NAME = 'Hinata_Files'
+COLLECTION_NAME = 'HanCock_Files'
 
 
 # --------- ʙᴏᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ---------
 SESSION = 'Media_search'
 API_ID = "18719789"
 API_HASH = "a03c27be3e14aac40f62cb4e95207fae"
-BOT_TOKEN = "5832308062:AAFoMhhBzIntupig2vQpl8oZ7MqGF3uUJwI" #"6091738037:AAGiTZ854KaXTUg3ZxAAkSmmYj5b9Fdqwgw"
+BOT_TOKEN = "6091738037:AAGiTZ854KaXTUg3ZxAAkSmmYj5b9Fdqwgw"
 
 
 # --------- ᴀᴅᴍɪɴs ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴜsᴇʀs ---------
 ADMINS = [5709622852, 5416887843, 5807975896]
-CHANNELS = [-1001819241311]
+CHANNELS = [-1001569112096]
 auth_users = []
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 AUTH_CHANNEL = -1001831916389
@@ -47,28 +47,14 @@ URL_SHORTNER_API_KEY = environ.get("URL_SHORTNER_API_KEY", "9e057515d222131456b5
 
 # --------- ᴘɪᴄs ʟɪɴᴋs ---------
 default_pics_links = """
-https://graph.org/file/f24fbacc3927a9876a5b0.jpg
-
-https://graph.org/file/a27705e0576079e7ad3be.jpg
-
-https://graph.org/file/6f6571b3e40119663cf7e.jpg
-
-https://graph.org/file/ae1c0b862bcc9c5c3ddf5.jpg
-
-https://graph.org/file/2191501943b1d071e3381.jpg
-
-https://graph.org/file/13efa9bfd072d82d6a6e9.jpg
-
-https://graph.org/file/ded5ae7cf8bd09779a696.jpg
-
-https://graph.org/file/71040f4d0eb67d382f834.jpg
+https://te.legra.ph/file/a30df60e107e11363cb08.jpg
 """
 PICS = (environ.get('PICS', default_pics_links)).split()
 
 
 # --------- sᴛᴀʀᴛ ᴍᴇsᴀᴀɢᴇs ---------
 default_start_msg = """
-ʜᴇʟʟᴏ sɪʀ/ᴍᴅᴍ
+ʜᴇʟʟᴏ
 ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴍᴏᴠɪᴇs 
 ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ 
 ᴍᴀᴋᴇ ᴀᴅᴍɪɴ ᴀɴᴅ ᴇɴᴊᴏʏ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏᴠɪᴇs
